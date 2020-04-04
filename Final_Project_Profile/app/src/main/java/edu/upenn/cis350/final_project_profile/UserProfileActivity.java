e86ce7c136d7d235faf73c50f27a16a777a8ad75
rosasun <rosasun@users.noreply.github.com>
2020-04-04 09:28:25 -0600

    iter_1_profile
    First iteration profile page proj

---
+++ Final_Project_Profile/app/src/main/java/edu/upenn/cis350/final_project_profile/UserProfileActivity.java
@@ -0,0 +1,14 @@
+package edu.upenn.cis350.final_project_profile;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class UserProfileActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_user_profile);
+    }
+}
